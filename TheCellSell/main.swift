598b06fdceec93d8307556752bed43d463084f91
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-01 22:19:16 -0400

    done calculations. work on cents

--- TheCellSell/main.swift
+++ TheCellSell/main.swift
@@ -23,6 +23,10 @@ let day = Int(readLine()!)!
 print("Number of evening minutes?")
 let evening = Int(readLine()!)!
 
+// Get weekend minutes
+print("Number of weekend minutes?")
+let weekend = Int(readLine()!)!
+
 // PROCESS
 
 // Calculate cost for plan A
@@ -34,22 +38,38 @@ a += (day - 100) * 25
 // Add evening cost
 a += evening * 15
 
+// Add weekend cost
+a += weekend * 20
+
 // Calculate cost for plan B
 var b = 0
 
 // Add daytime cost
-b += (day - 250) * 45
+if day >= 250 {
+    b += (day - 250) * 45
+}
+else{b+=0}
 
 // Add evening cost
 b += evening * 35
 
+// Add weekend cost
+b += weekend * 25
+
+//Convert to a double to display cents nicely
+let c = Double(a/100)
+let d = Double(b/100)
 
 // OUTPUT
-print("Plan A costs \(a)")
-print("Plan B costs \(b)")
+print("Plan A costs \(c)")
+print("Plan B costs \(d)")
 
 if a > b {
     print("Plan B is cheapest.")
 } else {
     print("Plan A is cheapest.")
 }
+
+
+
+//work on cents
