02f57586b574ae033d5596a87a26506f3a67360e
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-05 11:36:44 -0400

    Cleaned up

--- ISpeakTXTMSG/main.swift
+++ ISpeakTXTMSG/main.swift
@@ -14,40 +14,40 @@ print("I Speak TXTMSG")
 print("==============")
 
 while 0<1 {
-// INPUT
+    // INPUT
 
-// Ask for a phrase
-print("Enter phrase> ", terminator: "")
-let phrase = readLine()!
-
-// PROCESS AND OUTPUT
-
-switch phrase {
-case "TA":
-    print("totally awesome")
-case "TTYL":
-    print("talk to you later")
-case "CU":
-    print("see you")
-case ":-)":
-    print("I’m happy")
-case ":-(":
-    print("I’m unhappy")
-case ";-)":
-    print("wink")
-case ":-P":
-    print("stick out my tongue")
-case "(˜.˜)":
-    print("sleepy")
-case "CCC":
-    print("Canadian Cheese Champion")
-case "CUZ":
-    print("because")
-case "TY":
-    print("thank you")
-case "YW":
-    print("you're welcome")
-default:
-    print(phrase)
-}
+    // Ask for a phrase
+    print("Enter phrase> ", terminator: "")
+    let phrase = readLine()!
+    
+    // PROCESS AND OUTPUT
+    
+    switch phrase {
+    case "TA":
+        print("totally awesome")
+    case "TTYL":
+        print("talk to you later")
+    case "CU":
+        print("see you")
+    case ":-)":
+        print("I’m happy")
+    case ":-(":
+        print("I’m unhappy")
+    case ";-)":
+        print("wink")
+    case ":-P":
+        print("stick out my tongue")
+    case "(˜.˜)":
+        print("sleepy")
+    case "CCC":
+        print("Canadian Cheese Champion")
+    case "CUZ":
+        print("because")
+    case "TY":
+        print("thank you")
+    case "YW":
+        print("you're welcome")
+    default:
+        print(phrase)
+    }
 }
