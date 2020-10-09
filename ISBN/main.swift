3604020a1d5ee614809dfedfb14fcb3679a39797
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-09 10:52:05 -0400

    ISBN Numbers, done and commented

--- ISBN/main.swift
+++ ISBN/main.swift
@@ -19,26 +19,34 @@ print("====")
 let first10Digits = "9780921418"
 
 // Set the sum of just the first 10 digits, which are always 9780921418
-// TODO: Use puzzle description to determine how much to subtract.
-let first10DigitsSum = 120 - 0
+let first10DigitsSum = 91
 
 // Ask for the 11th digit
 print("Digit 11? ", terminator: "")
 let digit11 = Int(readLine()!)!
 
+// Ask for the 12th digit
 print("Digit 12? ", terminator: "")
 let digit12 = Int(readLine()!)!
 
+// Ask for the 13th digit
 print("Digit 13? ", terminator: "")
 let digit13 = Int(readLine()!)!
 
 // PROCESS & OUTPUT
 
 // Finish calculating the sum including the 11th, 12th, and 13th digits of the ISBN number
+let last3digitsSum = digit11 * 1 + digit12 * 3 + digit13 * 1
+let totalSum = first10DigitsSum + last3digitsSum
 
 // Print the 1-3 sum for the entire 13-digit ISBN number
+print("The 1-3 Sum is \(totalSum)")
 
-// Determine whether this entire ISBN number is valid,
-// using the special property of an ISBN number (see puzzle description).
-// HINT: Review the arithmetic operators tutorial from the Swift Fundamentals.
-//       https://www.hackingwithswift.com/sixty/3/1/arithmetic-operators
+// Determine whether this entire ISBN number is valid
+let last3digits = "\(digit11)\(digit12)\(digit13)"
+if totalSum % 10 == 0 {
+    print("The ISBN number 9780921418\(last3digits) is a valid ISBN number.")
+}
+else {
+    print("The ISBN number 9780921418\(last3digits) is not a valid ISBN number.")
+}
