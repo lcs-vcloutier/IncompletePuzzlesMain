6e7011b44bd0860149d37c386b84efe7fabdb489
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-20 14:09:44 -0400

    added extensions file

---
+++ Extensions.swift
@@ -0,0 +1,58 @@
+//
+//  Extensions.swift
+//  TheCellSell
+//
+//  Created by Cloutier, Vincent on 2020-10-20.
+//
+
+import Foundation
+
+extension Int {
+    
+    static func collectInput(withPrompt prompt: String, minimum: Int?, maximum: Int?) -> Int {
+        
+        // Loop until a valid value is provided
+        while true {
+            
+            // Prompt the user
+            print(prompt)
+            
+            // Collect the input
+            guard let givenInput = readLine() else {
+                continue
+            }
+            
+            // Convert to an integer
+            guard let givenInteger = Int(givenInput) else {
+                continue
+            }
+            
+            // If a lowest value for the integer was specified...
+            if let minimumValue = minimum {
+                
+                // ... then check that the given integer is greater than or equal to the lowest desired value.
+                guard givenInteger >= minimumValue else {
+                    continue
+                }
+                
+            }
+            
+            // If an highest possible value for the integer was specified...
+            if let maximumValue = maximum {
+                
+                // ... then check that the given integer is less than or equal to the highest desired value.
+                guard givenInteger <= maximumValue else {
+                    continue
+                }
+                
+                
+            }
+            
+            // If we've made it past all the checks, the input is an integer in the desired range of values, so, return it
+            return givenInteger
+            
+        }
+        
+    }
+    
+}
