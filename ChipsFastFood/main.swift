da210de1efc39f3689f8ecab2c3b95bb9624e72c
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-24 14:08:39 -0400

    set up variables etc. about to do logic with a  switch statement

--- ChipsFastFood/main.swift
+++ ChipsFastFood/main.swift
@@ -18,25 +18,28 @@ print("")
 print("Welcome to Chip's Fast Food Emporium")
 
 // INPUT
-
 // Get burger choice
-// NOTE: Remember to add Extensions.swift as a file that will be compiled for the ChipsFastFood target.
-//
-//       Here is how to do that:
-//       Select the blue IncompletePuzzles project file.
-//       Then select the ChipsFastFood target.
-//       Then select Build Phases from the tabs in the middle of the screen.
-//       Expand the Compile Sources subsection.
-//       Then drag and drop Extensions.swift to the Compile Sources subsection.
-//
 let burger = Int.collectInput(withPrompt: "Please enter a burger choice:", minimum: 1, maximum: 4)
-let drink = 3
-let side = 4
-let dessert = 1
+// Get drink choice
+let drink = Int.collectInput(withPrompt: "Please enter a drink choice:", minimum: 1, maximum: 4)
+// Get side choice
+let side = Int.collectInput(withPrompt: "Please enter a side choice:", minimum: 1, maximum: 4)
+// Get dessert choice
+let dessert = Int.collectInput(withPrompt: "Please enter a dessert choice:", minimum: 1, maximum: 4)
 
 // PROCESS
-// NOTE: Give some thought to what kind of data structure would be helpful for this program.
-//       Perhaps a collection type of some sort?
+// Get burger calories
+let burgerCalorie = 0
+// Get drink calories
+let drinkCalorie = 0
+// Get side calories
+let sideCalorie = 0
+// Get dessert calories
+let dessertCalorie = 0
+
+// Get total calorie count
+let totalCalorieCount = burgerCalorie + drinkCalorie + sideCalorie + dessertCalorie
 
 // OUTPUT
 // Show the calories total for the choices made by the user.
+print("Your total calorie count is \(totalCalorieCount)")
