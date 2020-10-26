3b24dba4c17afa7414e7b7ca3056955c383d09e8
Russell Gordon <rgordon@lcs.on.ca>
2020-10-26 14:27:18 -0400

    Added a couple of tests to the test plan.

--- TheCellSell/main.swift
+++ TheCellSell/main.swift
@@ -28,33 +28,69 @@ let day = Int(readLine()!)!
 print("Number of evening minutes?")
 let evening = Int(readLine()!)!
 
+// Get weekend minutes
+print("Number of weekend minutes?")
+let weekend = Int(readLine()!)!
+
 // PROCESS
+class CellSell {
+    
+    init () {
+        
+    }
+    
+    static func compareCosts(day: Int, evening: Int, weekend: Int) -> String {
+        
+        // Calculate cost for plan A
+        var a = 0
+        
+        // Add daytime cost
+        if day > 100 {
+            a += (day - 100) * 25
+        }
+        
+        // Add evening cost
+        a += evening * 15
+        
+        // Add weekend cost
+        a += weekend * 20
+        
+        // Calculate cost for plan B
+        var b = 0
+        
+        // Add daytime cost
+        if day > 250 {
+            b += (day - 250) * 45
+        }
+        
+        // Add evening cost
+        b += evening * 35
+        
+        // Add weekend cost
+        b += weekend * 25
+        
+        // OUTPUT
+        var result = ""
 
-// Calculate cost for plan A
-var a = 0
-
-// Add daytime cost
-a += (day - 100) * 25
-
-// Add evening cost
-a += evening * 15
-
-// Calculate cost for plan B
-var b = 0
-
-// Add daytime cost
-b += (day - 250) * 45
-
-// Add evening cost
-b += evening * 35
-
-
-// OUTPUT
-print("Plan A costs \(a)")
-print("Plan B costs \(b)")
-
-if a > b {
-    print("Plan B is cheapest.")
-} else {
-    print("Plan A is cheapest.")
+        result += "Plan A costs \(a)\n"
+        result += "Plan B costs \(b)\n"
+        
+        if a > b {
+            result += "Plan B is cheapest."
+        } else if a == b {
+            result += "Plan A and B are the same price."
+        } else {
+            result += "Plan A is cheapest."
+        }
+        
+        // Return the result
+        return result
+        
+    }
+    
 }
+
+// Use the new class to determine the result
+print(CellSell.compareCosts(day: day,
+                            evening: evening,
+                            weekend: weekend))
--- TheCellSellTests/TheCellSellTests.swift
+++ TheCellSellTests/TheCellSellTests.swift
@@ -1,32 +1,48 @@
 import XCTest
-//@testable import Doubler
 
-//class Doubler {
-//
-//    init (){
-//
-//    }
-//
-//    func double(n: Int) -> Int {
-//        return n * 2
-//    }
-//
-//}
-
-
-class DoublerTests: XCTestCase {
-
+class TheCellSellTests: XCTestCase {
+    
     override func setUp() {
         super.setUp()
     }
-
+    
     override func tearDown() {
         super.tearDown()
     }
-
-    func testDoubler() {
-        let x = Doubler()
-        XCTAssertEqual(10, x.double(n: 3))  // this should compile but fail the test
+    
+    // Typical case 
+    // • Daytime minutes more than free threshold for both plans
+    // • Plan A is cheapest
+    // • All output text exactly as shown
+    // • Amounts exactly as shown 
+    func testTypicalCaseA() {
+        
+        XCTAssertEqual("""
+            Plan A costs 8175
+            Plan B costs 8575
+            Plan A is cheapest.
+            """,
+                       CellSell.compareCosts(day: 300,
+                                             evening: 145,
+                                             weekend: 50))
+        
     }
 
+    // Typical case 
+    // • Daytime minutes more than free threshold for both plans
+    // • Plan B is cheapest 
+    func testTypicalCaseB() {
+        
+        XCTAssertEqual("""
+            Plan A costs 5600
+            Plan B costs 3940
+            Plan B is cheapest.
+            """,
+                       CellSell.compareCosts(day: 262,
+                                             evening: 90,
+                                             weekend: 10))
+        
+    }
+
+    
 }
