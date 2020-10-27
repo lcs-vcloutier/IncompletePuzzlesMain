4bc063c466c553ee08a904e0865d4552fd367464
Russell Gordon <rgordon@lcs.on.ca>
2020-10-27 09:45:56 -0400

    The Cell Sell exemplar solution and unit tests exemplar are added.

--- TheCellSellTestsExemplar/TheCellSellTestsExemplar.swift
+++ TheCellSellTestsExemplar/TheCellSellTestsExemplar.swift
@@ -44,5 +44,19 @@ class TheCellSellTests: XCTestCase {
         
     }
     
-    
+    // Typical case 
+    // • Daytime minutes more than free threshold for both plans
+    // • Plans A and B are same cost 
+    func testTypicalCaseC() {
+        
+        XCTAssertEqual("""
+            Plan A costs 7120
+            Plan B costs 7120
+            Plans A and B are the same price.
+            """,
+                       compareCosts(day: 296,
+                                    evening: 140,
+                                    weekend: 6))
+        
+    }
 }
