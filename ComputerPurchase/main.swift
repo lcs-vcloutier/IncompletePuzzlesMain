3d2ac48ff733c45a32d8d505d6fd45273ff90c07
Russell Gordon <rgordon@lcs.on.ca>
2020-10-27 20:45:18 -0400

    Added Computer Purchase puzzle.

---
+++ ComputerPurchase/main.swift
@@ -0,0 +1,35 @@
+//
+//  main.swift
+//  ComputerPurchase
+//
+//  Puzzle description available at:
+//
+//  https://www.russellgordon.ca/incomplete-puzzles/computer-purchase.pdf
+//
+//  NOTE: Puzzle adapted from material provided by the University of Waterloo.
+
+import Foundation
+
+print("Computer Purchase")
+print("=================")
+
+// HINT: There are many ways to approach this puzzle.
+//       How might you use a structure to complete this puzzle?
+
+// NOTE: Completing Magic Squares (or at least having tried the starter code)
+//       is recommended before trying this puzzle. 👀 at lines 28 and 36.
+
+// INPUT
+
+// Find out how many computers we will compare specs for
+print("How many specs will be provided?")
+let n = Int(readLine()!)!
+
+// Get the specs for "n" different computers
+
+
+// PROCESS
+
+
+// OUTPUT
+
