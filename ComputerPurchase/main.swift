560d0f61bcbd4f530d3b79d03196b979a1a72e26
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-29 21:53:02 -0400

    logic mostly done, dictionaries need to do output & clean up

--- ComputerPurchase/main.swift
+++ ComputerPurchase/main.swift
@@ -15,61 +15,67 @@ print("=================")
 
 // NOTE: Completing Magic Squares (or at least having tried the starter code)
 //       is recommended before trying this puzzle. 👀 at lines 28 and 36.
-//struct ComputerScore  {
-//
-//    // Regular properties to track RAM
-//    var name: String = ""
-//    // Regular properties to track RAM
-//    var r: Int = 0
-//    // Regular properties to track CPU speed
-//    var s: Int = 0
-//    // Regular properties to track Disk Drive Space
-//    var d: Int = 0
-//
-//    // Computed property to determine which computer is best (higher number the better)
-//    var totalPoints: Int {
-//        return r * 2 + s * 3 + d
-//    }
-//}
+struct ComputerScore  {
+    
+    // Regular properties to track NAME
+    var name: String
+    // Regular properties to track RAM
+    var r: Int
+    // Regular properties to track CPU speed
+    var s: Int
+    // Regular properties to track Disk Drive Space
+    var d: Int
+    
+    // Computed property to determine which computer is best (higher number the better)
+    var totalPoints: Int {
+        return r * 2 + s * 3 + d
+    }
+}
 // INPUT
 
 // Find out how many computers we will compare specs for
 let numberOfSpecs =  Int.collectInput(withPrompt: "How many specs will be provided?", minimum: 0, maximum: 10)
 
-// Get the specs for "n" different computers
-for i in 0...numberOfSpecs - 1 {
-    print("Spec \(i+1)?")
-    var line1 = readLine()!
-    print(line1)
+var roles = ["": 0]
+
+func getInput(arrayName:String, inputName:String) {
+    var arrayName = [""]
+    let inputName = readLine()!
+    arrayName.append(contentsOf: inputName.components(separatedBy: " "))
+    roles[arrayName[1]] = Int(arrayName[2])! * 2 + Int(arrayName[3])! * 3 + Int(arrayName[4])!
 }
-var numbers: [[String]] = []
-var comp1 = readLine()!
-numbers.append(comp1.components(separatedBy: " "))
-
-
+for i in 0...numberOfSpecs {
     
-var comp3 = readLine()!
-numbers.append(comp3.components(separatedBy: " "))
-
-var comp4 = readLine()!
-numbers.append(comp4.components(separatedBy: " "))
-
-func getInput(varName:String) {
-    let varName = readLine()!
-    numbers.append(varName.components(separatedBy: " "))
+    print("Spec \(i)?")
+    getInput(arrayName: "array\(i)", inputName: "input\(i)")
 }
 
-// INPUT
-// Get apples data
+var testVar = roles.values.max()
 
-// Get bananas data
-print(numbers)
+print("The best computer for you \(roles[testVar])")
 
-// PROCESS
-//print(comp1.totalPoints)
-//print(comp2.totalPoints)
-//for _ in numbers
-// OUTPUT
-print("The best computer for you is \(numberOfSpecs)")
+//// Get the specs for "n" different computers
+//var inputArray =  [[]]
+//
+//func getInput(varName:String, computerName:String, n:Int) {
+//    let varName = readLine()!
+//    inputArray.append(varName.components(separatedBy: " "))
+//    let computerName = ComputerScore(name: inputArray[n], r: Int(inputArray[n+1])!, s:Int(inputArray[n+2])!, d:Int(inputArray[n+3])!)
+//    print(computerName)
+//}
+//
+//for i in 0...numberOfSpecs - 1 {
+//    print("Spec \(i+1)?")
+//    getInput(varName: "Spec\(i+1)", computerName: "comp\(i+1)", n: i)
+//}
 
-
+//print(inputArray)
+//
+//// PROCESS
+////print(comp1.totalPoints)
+////print(comp2.totalPoints)
+////for _ in numbers
+//// OUTPUT
+//print("The best computer for you is \(numberOfSpecs)")
+//
+//
